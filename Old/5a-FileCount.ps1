a030bd8d456ede360a980a74efd42c273c901d98
Mike F <mike@mikefal.net>
2015-10-03 14:33:38 -0600

    Updated PDF and removed OLD directory

--- Old/5a-FileCount.ps1
+++
@@ -1,9 +0,0 @@
-﻿param([string]$PathName)
-
-if(Test-Path -Path $PathName){
-    $FileCount = (Get-ChildItem $PathName).Length
-    "$PathName has $FileCount files" | Out-Host
-}
-else{
-    Write-Warning "Path is invalid"
-}
